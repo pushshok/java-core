e3d2028025652a90ec9684680fab1646f5e77d11
pushshok <pushshok@gmail.com>
2022-01-25 16:20:54 +0300

    Create classes Generalization for work with arrays and classes Box, Fruit, Apple, Orange, BoxTypeException for homework 3

---
+++ homework3/Apple.java
@@ -0,0 +1,13 @@
+/**
+ * Java Core. Homework #3. Class Apple
+ * @author Zdibnyak Maxim
+ * @version 25.01.2022
+ */
+
+package ru.geekbrains.java_core;
+
+public class Apple extends Fruit {
+    public Apple (String name, float weight) {
+        super(name, weight);
+    }
+}
---
+++ homework3/Orange.java
@@ -0,0 +1,13 @@
+/**
+ * Java Core. Homework #3. Class Orange
+ * @author Zdibnyak Maxim
+ * @version 25.01.2022
+ */
+
+package ru.geekbrains.java_core;
+
+public class Orange extends Fruit {
+    public Orange (String name, float weight) {
+        super(name, weight);
+    }
+}
